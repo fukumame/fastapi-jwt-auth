84e5ef58dd07aa97e93575e0fa43a9dcd76fc303
fukumame <tanuma0115@gmail.com>
2023-11-24 22:58:42 +0900

    fix pydantic v2

--- fastapi_jwt_auth/config.py
+++ fastapi_jwt_auth/config.py
@@ -1,15 +1,15 @@
 from datetime import timedelta
-from typing import Optional, Union, Sequence, List
+from typing import Optional, Union, Sequence, List, Set
 from pydantic import (
     BaseModel,
     validator,
     StrictBool,
     StrictInt,
-    StrictStr
+    StrictStr, field_validator
 )
 
 class LoadConfig(BaseModel):
-    authjwt_token_location: Optional[Sequence[StrictStr]] = {'headers'}
+    authjwt_token_location: Optional[Set[StrictStr]] = {'headers'}
     authjwt_secret_key: Optional[StrictStr] = None
     authjwt_public_key: Optional[StrictStr] = None
     authjwt_private_key: Optional[StrictStr] = None
@@ -44,42 +44,46 @@ class LoadConfig(BaseModel):
     authjwt_refresh_csrf_header_name: Optional[StrictStr] = "X-CSRF-Token"
     authjwt_csrf_methods: Optional[Sequence[StrictStr]] = {'POST','PUT','PATCH','DELETE'}
 
-    @validator('authjwt_access_token_expires')
+    @field_validator('authjwt_access_token_expires')
     def validate_access_token_expires(cls, v):
         if v is True:
             raise ValueError("The 'authjwt_access_token_expires' only accept value False (bool)")
         return v
 
-    @validator('authjwt_refresh_token_expires')
+    @field_validator('authjwt_refresh_token_expires')
     def validate_refresh_token_expires(cls, v):
         if v is True:
             raise ValueError("The 'authjwt_refresh_token_expires' only accept value False (bool)")
         return v
 
-    @validator('authjwt_denylist_token_checks', each_item=True)
+    @field_validator('authjwt_denylist_token_checks')
     def validate_denylist_token_checks(cls, v):
-        if v not in ['access','refresh']:
-            raise ValueError("The 'authjwt_denylist_token_checks' must be between 'access' or 'refresh'")
+        for item in v:
+            if item not in ['access', 'refresh']:
+                raise ValueError("The 'authjwt_denylist_token_checks' must be between 'access' or 'refresh'")
         return v
 
-    @validator('authjwt_token_location', each_item=True)
+    @field_validator('authjwt_token_location')
     def validate_token_location(cls, v):
-        if v not in ['headers','cookies']:
-            raise ValueError("The 'authjwt_token_location' must be between 'headers' or 'cookies'")
+        for location in v:
+            if location not in ['headers', 'cookies']:
+                raise ValueError("The 'authjwt_token_location' must be between 'headers' or 'cookies'")
         return v
 
-    @validator('authjwt_cookie_samesite')
+    @field_validator('authjwt_cookie_samesite')
     def validate_cookie_samesite(cls, v):
-        if v not in ['strict','lax','none']:
-            raise ValueError("The 'authjwt_cookie_samesite' must be between 'strict', 'lax', 'none'")
+        for item in v:
+            if item not in ['strict', 'lax', 'none']:
+                raise ValueError("The 'authjwt_cookie_samesite' must be between 'strict', 'lax', 'none'")
         return v
 
-    @validator('authjwt_csrf_methods', each_item=True)
+    @field_validator('authjwt_csrf_methods')
     def validate_csrf_methods(cls, v):
-        if v.upper() not in {"GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"}:
-            raise ValueError("The 'authjwt_csrf_methods' must be between http request methods")
+        for item in v:
+            if item.upper() not in {"GET", "HEAD", "POST", "PUT", "DELETE", "PATCH"}:
+                raise ValueError("The 'authjwt_csrf_methods' must be between http request methods")
         return v.upper()
 
     class Config:
-        min_anystr_length = 1
-        anystr_strip_whitespace = True
+        str_min_length = 1
+        str_strip_whitespace = True
